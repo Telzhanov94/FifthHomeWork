57e6a69da79bae10c6155d1b7663648de771c403
Temirlan <telzhanov94@gmail.com>
2022-09-04 17:19:26 +0600

    46mc 0 kb

---
+++ Elephante/Program.cs
@@ -0,0 +1,17 @@
+﻿int Long=int.Parse(Console.ReadLine());
+int count=0;
+int res = Long;
+int i=5;
+while(Long!=0)
+{
+if (Long>=i)
+{
+    Long=Long-i;
+    count++;
+}
+else
+{
+    i--;
+}
+}
+Console.WriteLine(count);
