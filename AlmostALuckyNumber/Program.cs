4f29681c4aa0c0cb27614aa25356d242cc80493f
Temirlan <telzhanov94@gmail.com>
2022-09-04 19:08:58 +0600

    Lucky number

---
+++ AlmostALuckyNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿string number = Console.ReadLine()??"0";
+int count=0;
+for (int i=0; i<number.Length;i++)
+{
+    if (number[i]=='4' | number[i]=='7')
+    {
+        count++;
+    }
+}
+if (count==4 | count ==7)
+{
+    Console.WriteLine("YES");
+}
+else {Console.WriteLine ("NO");}
