1a1505c133a4d05d905c823a7122227bcea683cf
Temirlan <telzhanov94@gmail.com>
2022-09-04 18:24:42 +0600

    That's right

---
+++ Years/Program.cs
@@ -0,0 +1,10 @@
+﻿string fy=Console.ReadLine() ??"0";
+int year=Convert.ToInt32(fy.ToString());
+year=year+1;
+fy=Convert.ToString(year);
+while (fy[0]==fy[1] | fy[0]==fy[2] | fy[0]==fy[3] | fy [1]== fy[2] | fy[1]==fy[3] | fy[2]==fy[3])
+{
+    year=year+1;
+    fy=Convert.ToString(year);
+}
+Console.WriteLine(fy);
