97331bbbae8621b8f68fe740eb196efbf0f220bb
Temirlan <telzhanov94@gmail.com>
2022-09-04 16:04:36 +0600

    Театральная Площадь

---
+++ Square/Program.cs
@@ -0,0 +1,7 @@
+﻿string str=Console.ReadLine()??"0";
+string [] s=str.Split(' ');
+decimal[] a=new decimal [3];
+for (int i=0;i<3;i++)
+a[i]=Convert.ToInt32(s[i].ToString());
+decimal d=(Math.Ceiling(a[0]/a[2])*Math.Ceiling(a[1]/a[2]));
+Console.Write(d);
