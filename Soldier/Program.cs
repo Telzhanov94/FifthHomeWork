f95314ebf10fba41d10ae6e3f847b8dcfcad7d39
Temirlan <telzhanov94@gmail.com>
2022-09-04 21:34:31 +0600

    ready

---
+++ Soldier/Program.cs
@@ -0,0 +1,11 @@
+﻿string soldier = Console.ReadLine()??"0";
+string [] money = soldier.Split(' ');
+int price = Convert.ToInt32(money[0].ToString());
+int HaveMoney = Convert.ToInt32(money[1].ToString());
+int quantity = Convert.ToInt32(money[2].ToString());
+int CostBabans = 0;
+for (int i=1;i<=quantity;i++)
+CostBabans = CostBabans + (price * i);
+int friendMoney = CostBabans - HaveMoney ;
+if (friendMoney<0) friendMoney=0;
+Console.WriteLine(friendMoney);
